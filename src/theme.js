9c3ca3bfcfcdea029728538a53878562c72bc4a1
GwMamba <gerald.mwang@gmail.com>
2024-02-05 22:58:50 +0900

    dark and light theme setting

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
+import { colors } from "@mui/material";
 
 // color design tokens
 export const tokens = (mode) => ({
@@ -119,3 +120,46 @@ export const tokens = (mode) => ({
     }
   )
 });
+
+// mui theme settings
+export const themeSettings = (mode) => {
+  const colors = tokens(mode);
+
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === 'dark'
+      ? {
+        primary: {
+          main: colors.primary[500],
+        },
+        secondary: {
+          main: colors.greenAccent[500],
+        },
+        neutral: {
+          dark: colors.grey[700],
+          main: colors.grey[500],
+          light: colors.grey[100]
+        },
+        background: {
+          default: colors.primary[500],
+        }
+        } : {      
+          primary: {
+            main: colors.primary[100],
+          },
+          secondary: {
+            main: colors.greenAccent[500],
+          },
+          neutral: {
+            dark: colors.grey[700],
+            main: colors.grey[500],
+            light: colors.grey[100]
+          },
+          background: {
+            default: "#fcfcfc",
+          }  
+        }),
+    },
+  };
+};
