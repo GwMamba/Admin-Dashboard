bd13712222cd5bdd9b8c7936b228ddf6dfc81fbd
GwMamba <gerald.mwang@gmail.com>
2024-02-05 23:44:06 +0900

    correct export details

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,5 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
-import { colors } from "@mui/material";
 
 // color design tokens
 export const tokens = (mode) => ({
@@ -197,7 +196,7 @@ export const ColorModeContext = createContext({
   toggleColorMode: () => {}
 });
 
-export const UseMode = () => {
+export const useMode = () => {
   const [mode, setMode] = useState("dark");
 
   const colorMode = useMemo(
