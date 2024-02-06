26a586bc5b693cf66b147ca1eedf776400deea36
GwMamba <gerald.mwang@gmail.com>
2024-02-06 19:07:43 +0900

    imports and topbar theme

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,34 @@
+import { Box, IconButton, useTheme } from '@mui/meterial';
+import { useContext } from 'react';
+import { ColorModeContext } from '../../theme';
+import InputBase from '@mui/material';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationOutlinedIcon from '@mui/icons-material/NotificationOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+
 const Topbar = () => {
-  return <div>Topbar</div>
-}
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* {Search Bar} */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+
+      </Box>
+      
+      <IconButton></IconButton>
+    </Box >
+
+  );
+};
 
 export default Topbar
